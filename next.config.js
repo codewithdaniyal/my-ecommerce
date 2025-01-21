63658e81730ad9cdd849264209853b9b4e513b4c
Daniyal Ahmed <datariq0311@gmail.com>
2025-01-21 23:56:09 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
-  }
-  
-  module.exports = nextConfig
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
+  typescript: {
+    // This will ignore TypeScript errors during build
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // This will ignore ESLint errors during build
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
