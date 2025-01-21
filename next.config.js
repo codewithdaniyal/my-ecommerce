e4e16d74f69f5ad5b8ee06a629f6bb0acee68a0a
Daniyal Ahmed <datariq0311@gmail.com>
2025-01-21 23:58:49 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -11,13 +9,11 @@ const nextConfig: NextConfig = {
     ],
   },
   typescript: {
-    // This will ignore TypeScript errors during build
     ignoreBuildErrors: true,
   },
   eslint: {
-    // This will ignore ESLint errors during build
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
